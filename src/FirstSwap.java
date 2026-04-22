e739d6192bde1f808e38c70728da2de290aa7560
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:24:22 +0400

    Add firstSwap method and tracking map for Task 9

--- src/FirstSwap.java
+++ src/FirstSwap.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 public class FirstSwap {
+    public static String[] firstSwap(String[] enteredWords) {
+        Map<String, Integer> firstSwapTracker = new HashMap<>();
+        return enteredWords;
+    }
     public static void main(String[] args){
 
     }
