ad7eb9aff1343c3f1b86848938fe7a050a248630
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 12:23:10 +0400

    Add firstChar method and map structure for Task 5

--- src/FirstCharacterWord.java
+++ src/FirstCharacterWord.java
@@ -1,6 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
 public class FirstCharacterWord {
+    public static Map<String, String> firstChar(String[] groupedWords) {
+        Map<String, String> groupedCharacterMap = new HashMap<>();
+        return groupedCharacterMap;
+    }
     public static void main (String[] args){
 
     }
