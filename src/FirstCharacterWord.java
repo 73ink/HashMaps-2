2826adbea05d7de99431290a373fd0b061567674
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 12:23:55 +0400

    Implement first character grouping logic in Task 5

--- src/FirstCharacterWord.java
+++ src/FirstCharacterWord.java
@@ -3,6 +3,15 @@ import java.util.Map;
 public class FirstCharacterWord {
     public static Map<String, String> firstChar(String[] groupedWords) {
         Map<String, String> groupedCharacterMap = new HashMap<>();
+        for (String currentWord : groupedWords) {
+            String firstCharacter = currentWord.substring(0, 1);
+
+            if (groupedCharacterMap.containsKey(firstCharacter)) {
+                groupedCharacterMap.put(firstCharacter, groupedCharacterMap.get(firstCharacter) + currentWord);
+            } else {
+                groupedCharacterMap.put(firstCharacter, currentWord);
+            }
+        }
         return groupedCharacterMap;
     }
     public static void main (String[] args){
