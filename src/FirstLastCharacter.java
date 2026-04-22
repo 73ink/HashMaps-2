dc3d1d3d1f19a5abf7f266fbb87114266d6959e2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 12:09:57 +0400

    Add pairs method and map structure for Task 3

--- src/FirstLastCharacter.java
+++ src/FirstLastCharacter.java
@@ -1,4 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
 public class FirstLastCharacter {
+    public static Map<String, String> pairs(String[] enteredWords){
+
+    Map<String, String> characterPairMap = new HashMap<>();
+        return characterPairMap;
+}
     public static void main (String[] args){
 
     }
