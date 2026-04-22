db22328c48dc34eb18a980ff95856ab0533de410
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 12:17:57 +0400

    Implement frequency counting logic in Task 4

--- src/WordFreqCount.java
+++ src/WordFreqCount.java
@@ -3,6 +3,13 @@ import java.util.Map;
 public class WordFreqCount {
     public static Map<String, Integer> wordCount(String[] wordsToCount) {
         Map<String, Integer> frequencyMap = new HashMap<>();
+        for (String currentWord : wordsToCount) {
+            if (frequencyMap.containsKey(currentWord)) {
+                frequencyMap.put(currentWord, frequencyMap.get(currentWord) + 1);
+            } else {
+                frequencyMap.put(currentWord, 1);
+            }
+        }
         return frequencyMap;
     }
 
