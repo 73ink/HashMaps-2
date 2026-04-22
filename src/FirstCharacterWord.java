b66a12e624c3afecd5a4200cfcfa4ba7689a2ef6
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 12:26:30 +0400

    Add examples and testing outputd, and finalize Task 5

--- src/FirstCharacterWord.java
+++ src/FirstCharacterWord.java
@@ -15,6 +15,8 @@ public class FirstCharacterWord {
         return groupedCharacterMap;
     }
     public static void main (String[] args){
-
+        System.out.println(firstChar(new String[]{"salt", "tea", "soda", "toast"}));
+        System.out.println(firstChar(new String[]{"aa", "bb", "cc", "aAA", "cCC", "d"}));
+        System.out.println(firstChar(new String[]{}));
     }
 }
