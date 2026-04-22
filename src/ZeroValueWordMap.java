035b07e1c31a70844f307f2459108d153318a6c1
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 10:47:47 +0400

    Implement zero value map logic in Task 1

--- src/ZeroValueWordMap.java
+++ src/ZeroValueWordMap.java
@@ -3,6 +3,9 @@ import java.util.Map;
 public class ZeroValueWordMap {
     public static Map<String, Integer> word0(String[] wordsFromList) {
         Map<String, Integer> zeroValueMap = new HashMap<>();
+        for (String currentWord : wordsFromList) {
+            zeroValueMap.put(currentWord, 0);
+        }
         return zeroValueMap;
     }
     public static void main(String[] args){
