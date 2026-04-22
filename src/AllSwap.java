7eadedba33ffc6e030b3df8ac533cb85475ad3a3
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:20:54 +0400

    Implement repeated swap logic in Task 8

--- src/AllSwap.java
+++ src/AllSwap.java
@@ -6,6 +6,17 @@ public class AllSwap {
         for (int index = 0; index < originalWords.length; index++) {
             String currentWord = originalWords[index];
             String firstCharacter = currentWord.substring(0, 1);
+            if (swapIndexMap.containsKey(firstCharacter)) {
+                int savedIndex = swapIndexMap.get(firstCharacter);
+
+                String temporaryWord = originalWords[savedIndex];
+                originalWords[savedIndex] = originalWords[index];
+                originalWords[index] = temporaryWord;
+
+                swapIndexMap.remove(firstCharacter);
+            } else {
+                swapIndexMap.put(firstCharacter, index);
+            }
         }
         return originalWords;
     }
