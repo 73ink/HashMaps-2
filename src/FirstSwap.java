a1af11706385a7793deb770d47c8781b4366dd34
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:24:49 +0400

    Add loop and first character reading in Task 9

--- src/FirstSwap.java
+++ src/FirstSwap.java
@@ -3,6 +3,10 @@ import java.util.*;
 public class FirstSwap {
     public static String[] firstSwap(String[] enteredWords) {
         Map<String, Integer> firstSwapTracker = new HashMap<>();
+        for (int index = 0; index < enteredWords.length; index++) {
+            String currentWord = enteredWords[index];
+            String firstCharacter = currentWord.substring(0, 1);
+        }
         return enteredWords;
     }
     public static void main(String[] args){
