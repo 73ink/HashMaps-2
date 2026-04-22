4acfd841660ab3a47dd841e74479c9b5c290c3b9
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:19:40 +0400

    Add allSwap method and swap map for Task 8

--- src/AllSwap.java
+++ src/AllSwap.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 public class AllSwap {
+    public static String[] allSwap(String[] originalWords) {
+        Map<String, Integer> swapIndexMap = new HashMap<>();
+        return originalWords;
+    }
     public static void main(String[] args) {
 
     }
