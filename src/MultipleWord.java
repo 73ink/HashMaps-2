6048e832cf2ac52845e71bd4385a46d73db9a777
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:15:48 +0400

    Implement repeated word status logic in Task 7

--- src/MultipleWord.java
+++ src/MultipleWord.java
@@ -4,6 +4,13 @@ import java.util.Map;
 public class MultipleWord {
     public static Map<String, Boolean> wordMultiple(String[] listedWords) {
         Map<String, Boolean> multipleStatusMap = new HashMap<>();
+        for (String currentWord : listedWords) {
+            if (multipleStatusMap.containsKey(currentWord)) {
+                multipleStatusMap.put(currentWord, true);
+            } else {
+                multipleStatusMap.put(currentWord, false);
+            }
+        }
         return multipleStatusMap;
     }
     public static void main (String[] args){
