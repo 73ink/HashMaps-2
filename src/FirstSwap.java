60a823abdbe19d2620d96cd31e16fe54eb0e0e9a
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:25:27 +0400

    Implement one-time swap logic in Task 9

--- src/FirstSwap.java
+++ src/FirstSwap.java
@@ -6,6 +6,19 @@ public class FirstSwap {
         for (int index = 0; index < enteredWords.length; index++) {
             String currentWord = enteredWords[index];
             String firstCharacter = currentWord.substring(0, 1);
+            if (firstSwapTracker.containsKey(firstCharacter)) {
+                int savedIndex = firstSwapTracker.get(firstCharacter);
+
+                if (savedIndex != -1) {
+                    String temporaryWord = enteredWords[savedIndex];
+                    enteredWords[savedIndex] = enteredWords[index];
+                    enteredWords[index] = temporaryWord;
+
+                    firstSwapTracker.put(firstCharacter, -1);
+                }
+            } else {
+                firstSwapTracker.put(firstCharacter, index);
+            }
         }
         return enteredWords;
     }
