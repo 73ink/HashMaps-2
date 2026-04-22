5c9d76d15379e152d5014bb2e0f23c3b28cb2aac
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 12:16:44 +0400

    Add wordCount method and map structure for Task 4

--- src/WordFreqCount.java
+++ src/WordFreqCount.java
@@ -1,6 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
 public class WordFreqCount {
+    public static Map<String, Integer> wordCount(String[] wordsToCount) {
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        return frequencyMap;
+    }
 
     public static void main (String[] args){
 
