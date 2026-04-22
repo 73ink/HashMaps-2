adfff120f5307d3992023bd0dcdd4415698dbfa2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:11:02 +0400

    Add wordAppend method and tracking map for Task 6

--- src/EvenAppearance.java
+++ src/EvenAppearance.java
@@ -2,6 +2,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EvenAppearance {
+    public static String wordAppend(String[] inputWords) {
+        Map<String, Integer> appearanceTracker = new HashMap<>();
+        String finalText = "";
+        return finalText;
+    }
     public static void main(String[] args) {
 
     }
