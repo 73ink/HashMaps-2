0bb27bad87321a88025f9f07e72f0354044eda49
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 10:47:09 +0400

    Add word0 method and map structure for Task 1

--- src/ZeroValueWordMap.java
+++ src/ZeroValueWordMap.java
@@ -1,4 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
 public class ZeroValueWordMap {
+    public static Map<String, Integer> word0(String[] wordsFromList) {
+        Map<String, Integer> zeroValueMap = new HashMap<>();
+        return zeroValueMap;
+    }
     public static void main(String[] args){
 
     }
