a9fb3a86579786b486405366e71d2e58d1f8c8dc
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:11:42 +0400

    Add word counting logic in Task 6

--- src/EvenAppearance.java
+++ src/EvenAppearance.java
@@ -5,6 +5,13 @@ public class EvenAppearance {
     public static String wordAppend(String[] inputWords) {
         Map<String, Integer> appearanceTracker = new HashMap<>();
         String finalText = "";
+        for (String currentWord : inputWords) {
+            if (appearanceTracker.containsKey(currentWord)) {
+                appearanceTracker.put(currentWord, appearanceTracker.get(currentWord) + 1);
+            } else {
+                appearanceTracker.put(currentWord, 1);
+            }
+        }
         return finalText;
     }
     public static void main(String[] args) {
