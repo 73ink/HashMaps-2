20c29dd7c6fe5a0980c4704d47e885f211e143d4
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:16:29 +0400

    Add sample outputs, and finalize Task 7

--- src/MultipleWord.java
+++ src/MultipleWord.java
@@ -14,6 +14,8 @@ public class MultipleWord {
         return multipleStatusMap;
     }
     public static void main (String[] args){
-
+        System.out.println(wordMultiple(new String[]{"a", "b", "a", "c", "b"}));
+        System.out.println(wordMultiple(new String[]{"c", "b", "a"}));
+        System.out.println(wordMultiple(new String[]{"c", "c", "c", "c"}));
     }
 }
