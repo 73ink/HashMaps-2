a3078091d6f6892c0cac4df1ab59b6d1bb6a3345
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 10:54:20 +0400

    Implement word length mapping logic in Task 2

--- src/WordLengthMap.java
+++ src/WordLengthMap.java
@@ -3,6 +3,9 @@ import java.util.Map;
 public class WordLengthMap {
     public static Map<String, Integer> wordLen(String[] inputWords) {
         Map<String, Integer> wordLengthMap = new HashMap<>();
+        for (String currentWord : inputWords) {
+            wordLengthMap.put(currentWord, currentWord.length());
+        }
         return wordLengthMap;
     }
     public static void main(String[] args) {
