cc49138f8eee5e5cbfff89db6589b303a48169f2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 10:53:37 +0400

    Add wordLen method and map structure for Task 2

--- src/WordLengthMap.java
+++ src/WordLengthMap.java
@@ -1,4 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
 public class WordLengthMap {
+    public static Map<String, Integer> wordLen(String[] inputWords) {
+        Map<String, Integer> wordLengthMap = new HashMap<>();
+        return wordLengthMap;
+    }
     public static void main(String[] args) {
 
     }
