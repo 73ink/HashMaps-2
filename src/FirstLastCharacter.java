2bb8844b53e59ae1131f1c58eb46e9e276c5949e
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 12:10:34 +0400

    Implement first and last character pair logic in Task 3

--- src/FirstLastCharacter.java
+++ src/FirstLastCharacter.java
@@ -4,6 +4,11 @@ public class FirstLastCharacter {
     public static Map<String, String> pairs(String[] enteredWords){
 
     Map<String, String> characterPairMap = new HashMap<>();
+        for (String currentWord : enteredWords) {
+            String firstCharacter = currentWord.substring(0, 1);
+            String lastCharacter = currentWord.substring(currentWord.length() - 1);
+            characterPairMap.put(firstCharacter, lastCharacter);
+        }
         return characterPairMap;
 }
     public static void main (String[] args){
