9d9ef294fd2792e81db9b6c36ae5d6dc9fd0507f
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-22 13:15:09 +0400

    Add wordMultiple method and map structure for Task 7

--- src/MultipleWord.java
+++ src/MultipleWord.java
@@ -2,6 +2,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MultipleWord {
+    public static Map<String, Boolean> wordMultiple(String[] listedWords) {
+        Map<String, Boolean> multipleStatusMap = new HashMap<>();
+        return multipleStatusMap;
+    }
     public static void main (String[] args){
 
     }
